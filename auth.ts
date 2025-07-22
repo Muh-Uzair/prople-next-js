aafd37e4f4e955926ebaecb1b6bb9e128ba33f5e
Muh-Uzair <muhammaduzair1062001@gmail.com>
2025-07-22 20:41:35 +0500

    sign google success

--- auth.ts
+++ auth.ts
@@ -3,4 +3,40 @@ import Google from "next-auth/providers/google";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  callbacks: {
+    async signIn({ user }) {
+      try {
+        const email = user?.email;
+
+        console.log(
+          "Hello----------------------------------------------------------",
+        );
+        console.log(process.env.NEXT_PUBLIC_BACK_END_URL);
+
+        const res = await fetch(
+          `${process.env.NEXT_PUBLIC_BACK_END_URL}/building-manager/create-building-manager-google`,
+          {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            credentials: "include",
+            body: JSON.stringify({
+              email: email,
+            }),
+          },
+        );
+
+        if (!res.ok) {
+          console.error("Failed to create user");
+          return false;
+        }
+
+        return true;
+      } catch (err: unknown) {
+        console.error("Error in signIn callback: Error => ", err);
+        return false;
+      }
+    },
+  },
 });
